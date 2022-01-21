d26813cd57ec9e6165ef16b88493795ca95ff71f
Alina Žegoda <78086348+AlinaZegoda@users.noreply.github.com>
2022-01-21 07:15:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+text = input("Ievadiet tekstu:")
+def replaceO (text):
+    if text.count("o")>0 or text.count("O")>0:
+        text = text.replace("o","%")
+        text = text.replace("O","%")
+        print(text)
+    else:
+        text = "Nav burtu O vai o."
+    return text
+    print(text)
+replaceO (text) 
+    
